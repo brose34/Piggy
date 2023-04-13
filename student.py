222d2707666afbfddcb0c633701cb329f910f136
Brandon Rose <125601362+brose34@users.noreply.github.com>
2023-04-13 18:59:56 +0000

    added rose menu item

--- student.py
+++ student.py
@@ -41,6 +41,7 @@ class Piggy(PiggyParent):
                 "f": ("Follow", self.follow),
                 "c": ("Calibrate", self.calibrate),
                 "q": ("Quit", self.quit)
+                "B": ("Rose" , self.Rose)
                 }
         # loop and print the menu...
         for key in sorted(menu.keys()):
@@ -55,7 +56,8 @@ class Piggy(PiggyParent):
     STUDENT PROJECTS
     ****************
     '''
-
+    def Rose(self):
+      print("This is for testing")
     def dance(self):
         """A higher-ordered algorithm to make your robot dance"""
         # TODO: check to see if it's safe before dancing
